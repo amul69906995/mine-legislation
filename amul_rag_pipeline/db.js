2385bc1ca615a9c9afeb7f39eef02383a2f9b4a6
amul kumar <amul.kumar.min21@itbhu.ac.in>
2025-10-24 01:53:21 +0530

    currently all text are in form of array we need to process this text

--- amul_rag_pipeline/db.js
+++
@@ -1,7 +0,0 @@
-const { Pinecone } =require('@pinecone-database/pinecone');
-require('dotenv').config({path:'../.env'})
-// console.log(process.env.PINECONE_API_KEY)
-const pc = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY
-});
-
